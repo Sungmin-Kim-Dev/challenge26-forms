c8bf110b10936eb61320e7f1e71187174af4386e
Sungmin_Kim <dreamerk24dev@gmail.com>
2025-05-04 22:43:33 +0900

    middleware

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,5 +1,10 @@
+// 위에는 사용자 이름 표시
+// bio 표시, 없으면 작성해보라는 문구
+// bio 편집 버튼
+// 편집 버튼 누르면 기존의 소개에서 편집해야 함
+// 나의 트윗 목록
+// 좋아요 목록
+
 export default function Profile() {
-  return (
-    <div>Profile</div>
-  );
+  return <div>Profile</div>;
 }
--- middleware.ts
+++ middleware.ts
@@ -5,27 +5,32 @@ interface Routes {
   [key: string]: boolean;
 }
 
+// 로그인 유저는 로그인 페이지, 계정 만들기 페이지 접근 시 메인 페이지("/")로 이동
+// 로그인 안 한 유저는 profile 페이지 등 보호된 페이지 접근 시 로그인 페이지("/login")로 이동
+// 로그인 페이지로 자동 이동
 // 오브젝트가 어레이보다 값을 찾기가 빠름.
 // array는 값을 찾으려면 하나씩 다 비교해야 함
+// publicOnlyUrls: 로그인하지 않은 사용자만 접근해야 하는 경로
+
 const publicOnlyUrls: Routes = {
-  "/": true,
-  "/login": true,
-  "/sms": true,
+  "/log-in": true,
   "/create-account": true,
 };
 
 export async function middleware(request: NextRequest) {
-  // const session = await getSession();
-  // const exist = publicOnlyUrls[request.nextUrl.pathname];
-  // if (!session.id) {
-  //   if (!exist) {
-  //     return NextResponse.redirect(new URL("/", request.url));
-  //   }
-  // } else {
-  //   if (exist) {
-  //     return NextResponse.redirect(new URL("/products", request.url));
-  //   }
-  // }
+  const session = await getSession();
+  const pathname = request.nextUrl.pathname;
+  const isPublicOnlyPath = publicOnlyUrls[pathname];
+
+  if (!session.id) {
+    if (pathname === "/profile") {
+      return NextResponse.redirect(new URL("/log-in", request.url));
+    }
+  } else {
+    if (isPublicOnlyPath) {
+      return NextResponse.redirect(new URL("/", request.url));
+    }
+  }
 }
 
 export const config = {
